ab121c3e20eb94cc1f1ec51f17c7c121fde1fe84
Patrick Wilson <pmwilson333@gmail.com>
2021-01-15 00:07:40 -0800

    css and app.js wired up

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from './styles.css';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className={styles.title}>Hello World</h1>;
 }
