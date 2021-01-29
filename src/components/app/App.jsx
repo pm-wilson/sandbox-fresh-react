2739fa600c2cfd3ebf6f1547929d159dc1908489
Patrick Wilson <pmwilson333@gmail.com>
2021-01-28 17:23:25 -0800

    finds a count in repeated string

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,5 +3,47 @@ import styles from './styles.css';
 
 
 export default function App() {
-  return <h1 className={styles.title}>Hello World</h1>;
+
+  const n = 11;
+  const s = 'aab';
+
+  let iterationACount = 0;
+
+  for(let i = 0; i < s.length; i++) {
+    if(s[i] === 'a') {
+      iterationACount += 1;
+    }
+  }
+
+  const equalTimesSInN = Math.floor(n / s.length);
+  const remainderModulo = n % s.length;
+
+
+  function findAsInString(string) {
+    let stringACount = 0;
+
+    for(let i = 0; i < string.length; i++) {
+      if(string[i] === 'a') {
+        stringACount += 1;
+      }
+    }
+    
+    return stringACount;
+  }
+
+  function findRemainderString(string, index) {
+    let newString = '';
+
+    for(let i = 0; i < index; i++) {
+      newString += string[i];  
+    }
+
+    return newString;
+  }
+
+
+  const count = (equalTimesSInN * iterationACount) + findAsInString(findRemainderString(s, remainderModulo));
+  console.log(count);
+
+  return null;
 }
